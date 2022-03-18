b958f2af4b778329137de39534d8aa46bf3394cb
gabrielamly <gabriela.molly@hotmail.com>
2022-03-18 19:47:40 +0530

    Return unique pointer of account type (current, savings) from account factory.

--- src/AccountFactory.cpp
+++ src/AccountFactory.cpp
@@ -5,13 +5,23 @@
  *      Author: Gabriela
  */
 
+#include <memory>
 #include "../interface/AccountFactory.h"
+#include "../header/CurrentAccount.h"
+#include "../header/SavingsAccount.h"
 
-std::unique_ptr<Account> AccountFactory::CreateAccount(int type)
+std::unique_ptr<Account> AccountFactory::CreateAccount(ACCOUNT_TYPE type)
 {
     if ( type == 1)
     {
         std::cout << "Account type = 1\n";
+        return std::make_unique<CurrentAccount>();
+    }
+
+    else if ( type == 2)
+    {
+    	std::cout << "Account type = 2\n";
+    	return std::make_unique<SavingsAccount>();
     }
     return nullptr;
 }
