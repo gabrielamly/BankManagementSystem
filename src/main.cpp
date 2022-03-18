0fc8a565275ce6f851e2e621ca62b036b0aca85f
gabrielamly <gabriela.molly@hotmail.com>
2022-03-18 19:49:26 +0530

    Fetch user choice to either open account or display the account holder information.

--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,62 @@
 #include <iostream>
 #include "../interface/AccountFactory.h"
 
+std::vector<PersonModel> Account::personData;
+
+bool openAccount()
+{
+
+    AccountFactory::CreateAccount(ACCOUNT_TYPE::CURRENT_ACCOUNT)->openAccount();
+    return true;
+}
+
+bool display()
+{
+    AccountFactory::CreateAccount(ACCOUNT_TYPE::CURRENT_ACCOUNT)->checkBalance();
+    return true;
+}
+
 int main()
 {
-	AccountFactory::CreateAccount(1);
-	return 0;
+    unsigned int userChoice = 0;
+    while(1)
+    {
+        std::cout << "1. New Account\n";
+        std::cout << "2. Check Balance\n";
+        std::cout << "6. Exit\n";
+
+        std::cin >> userChoice;
+
+        switch (userChoice)
+        {
+        case 1:
+        {
+            openAccount();
+            break;
+        }
+
+        case 2:
+        {
+            display();
+            break;
+        }
+
+        case 6:
+        {
+            std::cout << "Good Bye\n";
+            break;
+        }
+
+        default:
+        {
+            std::cout << "Wrong Choice\n";
+            break;
+        }
+        }
+
+    }
+
+
+    return 0;
 }
 
