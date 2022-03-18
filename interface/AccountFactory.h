b8bb0c1caa4a4c33ebe09db5d50b81e47e05a8ff
gabrielamly <gabriela.molly@hotmail.com>
2022-03-18 19:45:25 +0530

    Added Account Type

--- interface/AccountFactory.h
+++ interface/AccountFactory.h
@@ -14,10 +14,17 @@
 /* Factory Method.
  * This function will be called to access pointer of Different account types */
 
+/* enum for Account Type */
+enum ACCOUNT_TYPE
+{
+    CURRENT_ACCOUNT = 1,
+    SAVINGS_ACCOUNT
+};
+
 class AccountFactory
 {
 public:
-    static std::unique_ptr<Account> CreateAccount(int type);
+    static std::unique_ptr<Account> CreateAccount(ACCOUNT_TYPE type);
 };
 
 
